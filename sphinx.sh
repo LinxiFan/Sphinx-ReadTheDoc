d8d16aaa204cfc6f44ae784ab270e653e4ab9672
LinxiFan <jimfanspire@gmail.com>
2017-01-02 00:20:38 +0800

    Auto commit build files in Sphinx

--- sphinx.sh
+++ sphinx.sh
@@ -2,24 +2,51 @@
 # (c) 2016 Jim Fan
 # Automate the document building process
 #
-git-add-doc() {
-    git add *.html
-    git add _static
-    git add _sources
-    git add _modules
-    git add *.js
-    git add *.inv
+
+HTML_DIR=doc/build/html
+
+trim() {
+    echo -e "$1" | tr -d '[:space:]'
+}
+
+strlen() {
+    echo -ne "$1" | wc -m
+}
+
+gh-pages-commit() {
+    grep_output=$( git status -uno | grep 'nothing to commit' )
+    # test empty string
+    if [[ $grep_output = *[^[:space:]]* ]]; then
+        echo 'Commit to gh-pages branch ...'
+        git checkout gh-pages
+    else
+        echo ABORT: all changes must be commited before switching to "gh-pages" branch
+        exit 1
+    fi
+
+    if [ ! -d $HTML_DIR ]; then
+        echo ABORT: $HTML_DIR does not exist. Run ./sphinx.sh first.
+        git checkout master
+        exit 1
+    fi
+    cp -rf $HTML_DIR/* .
+    git add -f *.html
+    git add -f _static
+    git add -f _sources
+    git add -f _modules
+    git add -f *.js
+    git add -f *.inv
     touch .nojekyll # otherwise folders with underscore (_static) won't be loaded
-    git add .nojekyll
+    git add -f .nojekyll
     git commit -m 'Update github pages.'
     #git pusho origin gh-pages
+    git checkout master
 }
 
 
 case $1 in
 c) # commit
-    echo 'commit to gh-pages branch'
-    git checkout gh-pages && git-add-doc
+    gh-pages-commit
 ;;
 
 d)
